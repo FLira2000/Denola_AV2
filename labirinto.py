8337379582dca80bfb1efc0d9315ba59b5b937c2
Josias da Silva Junior <josiasdsj1@gmail.com>
2021-10-17 11:48:58 -0300

    update labirinto
    eu juro q n eh desespero

--- labirinto.py
+++ labirinto.py
@@ -6,39 +6,39 @@
 graph = {
     1: [2],
     2: [1, 6, 3],
-    6: [2],
+    6: [],
     3: [2, 4, 8],
     4: [3, 17],
-    17: [4],
+    17: [],
     8: [3, 9, 7],
-    9: [8],
+    9: [],
     7: [8, 12],
     12: [7, 11, 19], 
     11: [12, 18, 10],
-    18: [11],
+    18: [],
     10: [11, 5, 25],
-    5: [10],
+    5: [],
     25: [10, 30, 26], 
-    30: [25],
+    30: [],
     26: [25, 32],
     32: [26, 31, 33],
-    31: [32],
-    33: [32],
+    31: [],
+    33: [],
     19: [12, 20],
     20: [19, 28],
     28: [20, 27, 34],
     27: [28, 24],
     24: [23, 27],
-    23: [24],
+    23: [],
     34: [28, 35],
     35: [34, 22, 36],
     22: [35, 21],
     21: [22, 14, 29],
-    29: [21],   
+    29: [],   
     14: [21, 13],
     13: [14, 15],
     15: [13, 16],
-    16: [15],
+    16: [],
     36: [35]
 }
 #variaveis globais
@@ -52,42 +52,20 @@ root = {} #raiz do grafo
 levels = {}
 demarkers = {}
 articulators = {}
-visited = set()
+
 
 def deepSearchLabyrinth(graph, visited, node):
     if node not in visited:
-        print(node)
-        visited.add(node)
+        visited.append(node)
+        removeFromStack = True
         for nearRoot in graph[node]:
             deepSearchLabyrinth(graph, visited, nearRoot)
+            removeFromStack = False
+            break
+        if removeFromStack:
+            visited.pop()
+        return visited
 
-deepSearchLabyrinth(graph, visited, 36)
-
-# def CallRecursiveDfs(graph, vertex, level):
-#     global contIn, contOut, contInOut
-#     contIn += 1
-#     contInOut[vertex] = [contIn, None]
-#     level[vertex]
-#     countSons = 0
-#     for nextMove in graph.get(vertex):
-#         if not contInOut.get(nextMove):
-#             root[nextMove] = arest
-#             countSons += 1
-#             print('aresta', arest)
-#             if levels[NearRoot[nextMove]] < levels[NearRoot[vertex]]:
-#                 NearRoot[vertex] = NearRoot[nextMove]
-#             if nextMove in demarkers:
-#                 articulators.add(vertex)
-#         else:
-#             if not contInOut[vertex][1]:
-#                 if root[vertex] != nextMove:
-#                     print('retorno para', root[vertex])
-#                     if levels[nextMove] < levels[NearRoot[vertex]]:
-#                         root[vertex] = nextMove
-#                 else:
-#                     arest = [(vertex, nextMove)]
-#                     print('aresta')
-#         contInOut += 1
-#         if root[vertex] in (vertex, root[vertex]):
-#             demarkers.add(vertex)
-#             return countSons
+visited = deepSearchLabyrinth(graph, [] , 36)
+visited.reverse()
+print(visited)
